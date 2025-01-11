3db66336a67c9076c4a2e2992b3f8e3ebf14e520
Sajin PJ <sajinsjn4433@gmail.com>
2025-01-11 23:41:18 +0530

    created a moviecard component, and integrated and rendered in app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import './App.css'
+// import './App.css'
+import MovieCard from './components/MovieCard'
 
 function App() {
 
   return (
     <>
-      
+      <MovieCard movie={{title: "The Shawshank Redemption", url: "https://m.media-amazon.com/images/M/MV5BMDFkYTc0MGEtZmNhMC00ZDIzLWFmNTEtODM1ZmRlYWMwMWFmXkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_.jpg", release_date: "1994"}} />
     </>
   )
 }
--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,24 +1,23 @@
 import React from "react";
 
-const MovieCard = () => {
+const MovieCard = ({ movie }) => {
+  const onFavouriteClick = () => {
+    alert("clicked");
+  };
   return (
     <>
       <div className="movie-card">
         <div className="movie-poster">
           <img src="{movie.url}" alt="{movie.title}" />
           <div className="movie-overlay">
-            <button
-              className="favourite-btn"
-              onClick={() => {
-                handleFavouriteClick();
-                alert("clicked");
-              }}
-            >♥</button>
+            <button className="favourite-btn" onClick={onFavouriteClick}>
+              ♥
+            </button>
           </div>
         </div>
         <div className="movie-info">
-            <h3>{movie.title}</h3>
-            <p>{movie.release_date}</p>
+          <h3>{movie.title}</h3>
+          <p>{movie.release_date}</p>
         </div>
       </div>
     </>
