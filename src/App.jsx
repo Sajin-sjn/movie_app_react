dfb169f49460aa5f4b716e8faf551c702cfc8add
Sajin PJ <sajinsjn4433@gmail.com>
2025-01-12 21:46:45 +0530

    react router dom installed to route between

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,20 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 // import './App.css'
 import MovieCard from './components/MovieCard'
+import Favourites from './components/Favourites'
 import Home from './pages/Home'
+import { Routes, Route } from 'react-router-dom'
 
 function App() {
 
   return (
     <>
-      <Home />
+      <main className="main-content">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/favourites" element={<Favourites />} />
+        </Routes>
+      </main>
     </>
   )
 }
