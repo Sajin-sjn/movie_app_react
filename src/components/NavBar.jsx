a7fe29dbeec2667e2047e1e09c26e077f384c693
Sajin PJ <sajinsjn4433@gmail.com>
2025-01-12 21:53:26 +0530

    navbar component created

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const NavBar = () => {
+    return (
+        <div>
+            <nav className="navbar">
+                <div className="navbar-brand">
+                    <h1>Movie App</h1>
+                </div>
+                <div className="navbar-links">
+                    <Link to="/" className='nav-link'>Home</Link>
+                    <Link to="/favourites" className='nav-link'>Favourites</Link>
+                </div>
+            </nav>
+        </div>
+    );
+}
+
+export default NavBar;
