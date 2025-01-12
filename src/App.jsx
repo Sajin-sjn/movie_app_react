d75c869359c0596aa885e6c5b0efecc924bf2c93
Sajin PJ <sajinsjn4433@gmail.com>
2025-01-12 21:55:52 +0530

    router linked to page

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,17 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
 // import './App.css'
-import MovieCard from './components/MovieCard'
-import Favourites from './components/Favourites'
-import Home from './pages/Home'
-import { Routes, Route } from 'react-router-dom'
+import MovieCard from "./components/MovieCard";
+import Favourites from "./components/Favourites";
+import Home from "./pages/Home";
+import { Routes, Route } from "react-router-dom";
+import NavBar from "./components/NavBar";
 
 function App() {
-
   return (
     <>
+      <NavBar />
       <main className="main-content">
         <Routes>
           <Route path="/" element={<Home />} />
@@ -18,7 +19,7 @@ function App() {
         </Routes>
       </main>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
